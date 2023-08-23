cd44c821cfae8fdb7f3b590808522aa5ef79aaa9
ralima <rlprafa@gmail.com>
2023-08-23 13:10:23 -0400

    adding layout scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,67 @@
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/Products/Products";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Navbar from "./pages/components/navbar/Navbar";
+import Footer from "./pages/components/footer/Footer";
+import Menu from "./pages/components/menu/Menu";
+import Login from "./pages/login/Login";
+
+import "./styles/global.scss"
+
 function App() {
+  const Layout = () => {
+    return (
+        <div className="main">
+          <Navbar />
+          <div className="container">
+            <div className="menuContainer">
+              <Menu />
+            </div>
+            <div className="contentContainer">
+              <Outlet />
+            </div>
+
+          </div>
+
+          <Footer />
+
+        </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ]);
 
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.scss"
+const Footer = () => {
+    return(
+        <div className="footer">
+            <span>Pure Plate</span>
+            <span>Better food choices</span>
+        </div>
+    )
+}
+export default Footer
